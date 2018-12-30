5709fb40a617824cd2ca13f37356e52dc9cf38dc
simonepri <simonepri@outlook.com>
2018-12-30 14:59:49 +0100

    Implement cli

--- src/main/java/sudoku/App.java
+++ src/main/java/sudoku/App.java
@@ -1,11 +1,141 @@
 package sudoku;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+
+import java.io.File;
+import java.io.IOException;
+import java.math.BigInteger;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.function.Consumer;
+
+import java.nio.file.NoSuchFileException;
+
 public class App {
-  public String getGreeting() {
-    return "Hello world.";
+  public static class Args {
+    @Parameter(names = "--help", help = true)
+    public boolean help = false;
+
+    @Parameter(names={"--print", "-p"}, description = "Print solutions")
+    public boolean print = false;
+
+    @Parameter(names={"--sequential", "-s"}, description = "Disable parallelism")
+    public boolean sequential = false;
+
+    @Parameter(description = "<filename>[ <filename>]*")
+    public List<String> filenames = new ArrayList<>();
   }
 
-  public static void main(String[] args) {
-    System.out.println(new App().getGreeting());
+  private final boolean help;
+  private final boolean print;
+  private final boolean sequential;
+  private final List<String> filenames;
+
+  /**
+   * Default Constructor.
+   * @param args the arguments for the app.
+   */
+  public App(Args args) {
+    help = args.help;
+    print = args.print;
+    sequential = args.sequential;
+    filenames = new ArrayList<>(args.filenames);
+  }
+
+  /**
+   * Read a file containing a sudoku description and convert it to an int matrix.
+   * @param filename a path to a file or a filname in the cwd.
+   */
+  private int[][] parse(String filename) throws IOException {
+    List<int[]> matrix = new LinkedList<int[]>();
+
+    Scanner scanner = new Scanner(new File(filename));
+    while (scanner.hasNextLine()) {
+      String line = scanner.nextLine();
+      List<Integer> row = new LinkedList<>();
+      for (char c : line.toCharArray()) {
+        if (c == '.') {
+          row.add(0);
+        } else if ('0' <= c && c <= '9') {
+          row.add(c - '0');
+        } else {
+          throw new IllegalArgumentException("Invalid character found: " + c + " in " + filename + "\n");
+        }
+      }
+      matrix.add(
+        row.stream().mapToInt(Integer::intValue).toArray()
+      );
+    }
+    scanner.close();
+
+    return matrix.toArray(new int[0][]);
+  }
+
+  public BigInteger enumerate(Board board) {
+    return enumerate(board, null);
+  }
+
+  public BigInteger enumerate(Board board, Consumer<Board> onSolution) {
+    if (sequential) {
+      return SequentialSolver.enumerate(board, onSolution);
+    }
+    return ParallelSolver.enumerate(board, onSolution);
+  }
+
+  /**
+   * Run the app.
+   * @param out the app will call this when needs to send a message.
+   */
+  public int run(Consumer<String> out) {
+    if (help || filenames.size() == 0) {
+      out.accept(
+        "Usage: sudoku [options] <filenames>[,<filenames>]*\n"
+        + "  Options:\n"
+        + "    --help               Print usage\n"
+        + "    --print, -p          Print solutions\n"
+        + "    --sequential, -s     Disable parallelism\n"
+      );
+      return 0;
+    }
+
+    try {
+      for (String filename : filenames) {
+        Board board = new Board(parse(filename));
+        if (print) {
+          enumerate(board, b -> out.accept(b.toString()));
+        } else {
+          out.accept(enumerate(board).toString());
+        }
+      }
+    } catch (NoSuchFileException e) {
+      out.accept("No such file: " + e.getFile() + "\n");
+      return 1;
+    } catch (IllegalArgumentException e) {
+      out.accept(e.getMessage() + "\n");
+      return 2;
+    } catch (Exception e) {
+      out.accept("An unexpected error occurred: " + e.getMessage() + "\n");
+      return -1;
+    }
+
+    return 0;
+  }
+
+  /**
+   * Entry point of the app.
+   * @param argv the arguments.
+   */
+  public static void main(String[] argv) {
+    Args args = new Args();
+    JCommander jct = JCommander.newBuilder().addObject(args).build();
+    jct.parse(argv);
+
+    App main = new App(args);
+    System.exit(main.run(System.out::print));
   }
 }
