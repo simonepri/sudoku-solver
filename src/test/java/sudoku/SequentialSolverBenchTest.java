0c894161d8342f6e983c702b03ce2641fdd9f4b4
simonepri <simonepri@outlook.com>
2018-12-31 00:27:05 +0100

    Update SequentialSolverBenchTest.java

--- src/test/java/sudoku/SequentialSolverBenchTest.java
+++ src/test/java/sudoku/SequentialSolverBenchTest.java
@@ -193,7 +193,7 @@ public class SequentialSolverBenchTest {
       {1, 0, 0, 0, 0, 6, 0, 0, 0},
       {0, 0, 6, 0, 0, 0, 0, 0, 8},
       {0, 0, 0, 0, 1, 0, 0, 3, 0},
-      {2, 0, 0, 0, 0, 0, 4, 0, 0}
+      {2, 0, 0, 0, 0, 0, 0, 0, 0}
     });
     assertThat(SequentialSolver.enumerate(sudoku).toString()).isEqualTo("48794239");
   }
